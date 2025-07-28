7b652a023e2ef29e90bea804044929331130be5b
coding-scantech <coding.scantech@gmail.com>
2025-07-28 23:13:32 +0300

    Added puppeteer stuff

--- bot3.js
+++ bot3.js
@@ -35,7 +35,16 @@ const balances = {
 const waClient = new WhatsAppClient({
   puppeteer: {
     headless: true,
-    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    args: [
+      "--no-sandbox",
+      "--disable-setuid-sandbox",
+      "--disable-dev-shm-usage",
+      "--disable-accelerated-2d-canvas",
+      "--no-first-run",
+      "--no-zygote",
+      "--single-process",
+      "--disable-gpu",
+    ],
   },
 })
 
